2628fb2f56b36f1eb30a29e5c1aedea41b1b8c93
Damian Stępień <damian.stepien@me.com>
2018-09-09 13:00:39 +0200

    Entity tests

--- test/unit/Data/EntityTest.php
+++ test/unit/Data/EntityTest.php
@@ -2,9 +2,50 @@
 
 namespace Test\Unit\DataFlow\Data;
 
+use DataFlow\Data\Entity;
 use PHPUnit\Framework\TestCase;
 
 class EntityTest extends TestCase
 {
+    public function testGet()
+    {
+        $entity = new Entity(['testKey' => 'testValue']);
 
+        $this->assertEquals('testValue', $entity->get('testKey'));
+    }
+
+    public function testGetAll()
+    {
+        $entity = new Entity($propertiesArray = ['testKey' => 'testValue']);
+
+        $this->assertEquals($propertiesArray, $entity->getAll());
+
+        $entity->set('anotherKey', 'test');
+
+        $this->assertNotEquals($propertiesArray, $entity->getAll());
+    }
+
+    public function testSet()
+    {
+        $entity = new Entity();
+        $entity->set('testKey', 'testValue');
+
+        $this->assertEquals('testValue', $entity->get('testKey'));
+    }
+
+    public function testHas()
+    {
+        $entity = new Entity(['testKey' => 'testValue']);
+
+        $this->assertTrue($entity->has('testKey'));
+        $this->assertFalse($entity->has('anotherKey'));
+    }
+
+    public function testRemove()
+    {
+        $entity = new Entity(['testKey' => 'testValue']);
+        $entity->remove('testKey');
+
+        $this->assertFalse($entity->has('testKey'));
+    }
 }
