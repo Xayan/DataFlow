4a328b031b0a149d54ec0e7bdd944143d0d12c27
Damian Stępień <damian.stepien@me.com>
2018-09-09 15:22:02 +0200

    Export CSV to string function

---
+++ src/IO/CSV/Export.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DataFlow\IO\CSV;
+
+use DataFlow\Data\EntityCollection;
+
+class Export
+{
+    public static function toString(EntityCollection $entityCollection)
+    {
+
+    }
+}
---
+++ src/functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Converts an array into CSV string
+ * Source: https://stackoverflow.com/questions/16352591/convert-php-array-to-csv-string
+ *
+ * @param $input
+ * @param string $delimiter
+ * @param string $enclosure
+ * @param string $escape
+ * @return string
+ */
+function str_putcsv($input, $delimiter = ',', $enclosure = '"', $escape = '\\')
+{
+    $fp = fopen('php://memory', 'r+');
+
+    fputcsv($fp, $input, $delimiter, $enclosure, $escape);
+
+    rewind($fp);
+    $data = rtrim(stream_get_contents($fp), "\n");
+    fclose($fp);
+
+    return rtrim($data, "\n");
+}
