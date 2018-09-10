5832a166abbb3d483300b1691f40041d3e7f6fac
Damian Stępień <damian.stepien@me.com>
2018-09-10 21:14:12 +0200

    Quiet mode test

--- test/Unit/IO/CSV/ExportTest.php
+++ test/Unit/IO/CSV/ExportTest.php
@@ -46,4 +46,27 @@ class ExportTest extends DataFlowTestCase
 
         $this->assertEquals($expectedContent, $content);
     }
+
+    public function testToString_quiet()
+    {
+        $people = [
+            $this->createDummyEntity(['id' => 1, 'name' => 'John Doe', 'age' => 12]),
+            $this->createDummyEntity(['id' => 2, 'name' => 'Johnny Bravo', 'age' => 30]),
+            $this->createDummyEntity(['id' => 3, 'name' => 'Jimmy', 'age' => 40])
+        ];
+
+        $peopleCollection = new EntityCollection($people);
+
+        $exportPolicy = new ExportPolicy();
+        $exportPolicy->setIncludeHeader(true);
+        $exportPolicy->setQuiet(true);
+        $exportPolicy->addColumnDefinition(new ColumnDefinition(0, 'id'));
+        $exportPolicy->addColumnDefinition(new ColumnDefinition(1, 'nonexistent'));
+        $exportPolicy->addColumnDefinition(new ColumnDefinition(2, 'age'));
+
+        $content = Export::toString($peopleCollection, $exportPolicy);
+        $expectedContent = file_get_contents('test/Resources/CSV/ExportTest_testToString_quiet_output.csv');
+
+        $this->assertEquals($expectedContent, $content);
+    }
 }
